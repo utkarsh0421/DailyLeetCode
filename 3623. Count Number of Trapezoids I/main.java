cab0dde2a2d1ad799a244cd1c8b6d5d2e650bbb0
utkarsh0421 <utkarshvidyarthi04@gmail.com>
2025-12-02 13:25:10 +0530

    Add files via upload

---
+++ 3623. Count Number of Trapezoids I/main.java
@@ -0,0 +1,26 @@
+// 3623. Count Number of Trapezoids I
+// https://leetcode.com/problems/count-number-of-trapezoids-i/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class main{
+    class Solution {
+    public int countTrapezoids(int[][] points) {
+        int n = points.length, MOD = 1000000007;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        long res = 0, total = 0;
+        for (int i = 0, j; i < n; i = j){
+            j = i + 1;
+            while (j < n && points[i][1] == points[j][1]){
+                j++;
+            }
+            long count = j - i;
+            long lines = count * (count - 1) / 2;
+            res = (res + total * lines) % MOD;
+            total = (total + lines) % MOD;
+        }
+        return (int)res;
+    }
+}
+}
